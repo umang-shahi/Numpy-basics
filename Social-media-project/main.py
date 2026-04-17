4faef20f6130ec5aba50c19553daac728640b47a
umang <umangshahi800@gmail.com>
2026-04-17 22:49:01 +0545

    Added dataset

---
+++ Social-media-project/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data.csv")
+
+print(df.head())
+
+avg_age = np.mean(df["age"])
+print("Average Age:", avg_age)
+
+print("Max Age:", np.max(df["age"]))
+print("Min Age:", np.min(df["age"]))
+
+total_addiction = np.sum(df["addiction_level"])
+print("Total Addiction Score:", total_addiction)
+
+std = np.std(df["addiction_level"])
+print("Std Dev:", std)
+
+age_array = df["age"].values
+print(age_array)
+
+high_addiction = df[np.array(df["addiction_level"]) > 5]
+print(high_addiction.head())
